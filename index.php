ef0422d81443edeedc187f745af79adcb179acef
McReiz <xreizonx@gmail.com>
2015-12-27 18:28:48 -0430

    construyendo las bases apartir de blank-3.3.0

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
-<?php
-    $dir_templ = $this->baseurl ."/templates/". $this->template;
-?>
+<?php 
+    defined( '_JEXEC' ) or die( 'Restricted access' ); 
+
+    include_once JPATH_THEMES.'/'.$this->template.'/logic.php';
+?> 
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+<html lang="<?php echo $this->language; ?>">
     <head>
         <jdoc:include type="head" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -12,13 +13,23 @@
     </head>
     <body>
         <header id="main-header">
-            <jdoc:include type="modules" name="Header" style="xhtml" />
-            <jdoc:include type="module" name="mainmenu" title="Main Menu" />
+            <div id="wrap">
+                <div class="head-info">
+                    <jdoc:include type="modules" name="HeaderLogo" style="xhtml" />
+                </div>
+                <nav class="head-menu">
+                    <jdoc:include type="modules" name="HeaderMenu" style="xhtml" />
+                <nav>
+            </div>
         </header>
+        <section id="main-slider">
+            <jdoc:include type="modules" name="Slider"
+        </section>
         <main id="main-content">
-            <article id="posts">
-                <jdoc:include type="modules" name="Articles" style="xhtml" />
-            </article>
+            <section id="posts">
+                <jdoc:include type="message" />
+                <jdoc:include type="component" />
+            </section>
             <aside id="sidebar">
                 <jdoc:include type="modules" name="Sidebar" style="xhtml" />
             </aside>
---
+++ logic.php
@@ -0,0 +1,20 @@
+<?php defined( '_JEXEC' ) or die;
+
+// variables
+$app = JFactory::getApplication();
+$doc = JFactory::getDocument();
+$menu = $app->getMenu();
+$active = $app->getMenu()->getActive();
+$params = $app->getParams();
+$pageclass = $params->get('pageclass_sfx');
+$tpath = $this->baseurl.'/templates/'.$this->template;
+
+// generator tag
+$this->setGenerator(null);
+
+// template js
+$doc->addScript($tpath.'/scripts/logic.js');
+
+// template css
+//$doc->addStyleSheet($tpath.'/css/template.css.php');
+ $dir_templ = $this->baseurl ."/templates/". $this->template;
